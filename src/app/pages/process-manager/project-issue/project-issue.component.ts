02e2f36ed38031e50c7b3f4381fbf7984c78660c
ThangNV <thangviet222@gmail.com>
2020-02-14 14:15:12 +0700

    commit

--- src/app/pages/process-manager/project-issue/project-issue.component.ts
+++ src/app/pages/process-manager/project-issue/project-issue.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
+
+declare var require: any;
+let Boost = require('highcharts/modules/boost');
+let noData = require('highcharts/modules/no-data-to-display');
+let More = require('highcharts/highcharts-more');
+
+Boost(Highcharts);
+noData(Highcharts);
+More(Highcharts);
+noData(Highcharts);
 
 @Component({
   selector: 'ngx-project-issue',
@@ -7,9 +18,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectIssueComponent implements OnInit {
 
+  public options: any = {
+    chart: {
+      type: 'bar'
+    },
+    title: {
+      text: 'Stacked bar chart'
+    },
+    xAxis: {
+      categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: 'Total fruit consumption'
+      }
+    },
+    legend: {
+      reversed: true
+    },
+    plotOptions: {
+      series: {
+        stacking: 'normal'
+      }
+    },
+    series: [{
+      name: 'John',
+      data: [5, 3, 4, 7, 2]
+    }, {
+      name: 'Jane',
+      data: [2, 2, 3, 2, 1]
+    }, {
+      name: 'Joe',
+      data: [3, 4, 4, 2, 5]
+    }]
+  }
   constructor() { }
 
   ngOnInit() {
+    Highcharts.chart('container', this.options);
   }
 
 }
